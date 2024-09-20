711283cdf07dab12cac89fc249f579c44a1965ef
Naseel Niyas <codenas07@gmail.com>
2024-09-20 21:00:53 +0300

    part 2

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
 print("Hello World")
+
+# String
+my_string = "naseel"
+print(my_string)
+my_string = "hello"
+print(my_string)
+
+# Integer
+my_int = 7
+print(my_int)
+print(type(my_int))
+
+# Floats
+my_float = 45.545
+print(my_float)
+print(type(my_float))
+
+# Booleans
+my_bool = True
+print(my_bool)
+print(type(my_bool))
